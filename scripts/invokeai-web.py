9939e3abead33ce6dc2d11c7e56f28a5abd14850
weaponlion2 <weaponlion2@gmail.com>
2025-08-21 12:11:23 +0530

    Update invokeai-web.py

--- scripts/invokeai-web.py
+++ scripts/invokeai-web.py
@@ -3,7 +3,7 @@ def main():
     import sys
     import os
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+    print(sys.path)
     from invokeai.app.api_app import invoke_api
 
     invoke_api()
