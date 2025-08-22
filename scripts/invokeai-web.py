c41e56a61d524a8992241cab20c648b714d549bb
weaponlion2 <weaponlion2@gmail.com>
2025-08-22 17:29:56 +0530

    Update invokeai-web.py

--- scripts/invokeai-web.py
+++ scripts/invokeai-web.py
@@ -2,8 +2,10 @@ import sys
 import os
 
 def main():
+    v1_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'invokeai/app'))
+    sys.path.append(v1_path)
     #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-    print(sys.path)
+    
     # Change working directory to the repo root
     os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
     from invokeai.app.api_app import invoke_api
